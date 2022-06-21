81e717777d2b908040fc638539c1057d5a349360
Anton Rudenkov <anton.r@ventor.tech>
2022-06-21 20:11:12 +0300

    [ADD] Added form to be able to resize the world. Added requirements.txt and .gitignore files

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from game_of_life import GameOfLife
 
 app = Flask(__name__)
@@ -7,16 +7,24 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index/')
 def index():
-    GameOfLife(width=25, height=25)
     return render_template('index.html')
 
 
-@app.route('/live/')
+@app.route('/live/', methods=['GET', 'POST'])
 def live():
     game = GameOfLife()
+
+    width = int(request.form.get('width', 10))
+    height = int(request.form.get('height', 10))
+
+    if len(game.world[0]) != width or len(game.world) != height and (width > 0 and height > 0):
+        game = GameOfLife(width=width, height=height)
+
     if game.counter > 0:
         game.form_new_generation()
+
     game.counter += 1
+
     return render_template('live.html', game=game)
 
 
